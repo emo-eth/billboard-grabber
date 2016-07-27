bfe77b91f17fc3de51c8860fc51a678e3501abbf
jameswenzel <wenzel.james.r@gmail.com>
2016-07-27 17:30:21 -0400

    script now uses quantize feature in billboard.py and real multiprocessing

--- script.py
+++ script.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python3
+import sys
+import billboard
+from jwp.jwmultithreaded import *
+from jwp.jwcache import *
+from datetime import date, timedelta
 
-from ChartGrabber import ChartGrabber
+cache = {}
 
 
-ChartGrabber(start_date='1958-01-01', charts_to_load=('hot-100',))
+def get_thing(chart, d):
+    if chart in cache:
+        if d in cache[chart]:
+            print(chart, d, 'retrieved from cache')
+            return {}
+    print('Downloading', chart, d)
+    return {chart: {d: billboard.ChartData(chart, d).to_JSON()}}
+
+
+def main(c):
+    start = date(1958, 8, 9)
+    delta = date.today() - start
+
+    ds = []
+    for x in range(0, delta.days + 11, 7):
+        d = start + timedelta(days=x)
+        ds.append([c, str(d)])
+
+    results = multithread(get_thing, ds)
+    for result in results:
+        for chart in result:
+            if chart not in cache:
+                cache[chart] = {}
+            cache[chart].update(result[chart])
+
+    write_cache('bbCache.json', cache)
+
+if __name__ == "__main__":
+    cache = load_cache('bbCache.json')
+    print
+    main(sys.argv[1])
